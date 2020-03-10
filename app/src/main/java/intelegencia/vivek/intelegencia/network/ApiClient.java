3ec8212c2657bdac35677f7535b1f6386ef97d03
vivek chandran <vivekc.calicut@gmail.com>
2020-03-10 13:03:47 +0530

    Initial appconfig

---
+++ app/src/main/java/intelegencia/vivek/intelegencia/network/ApiClient.java
@@ -0,0 +1,31 @@
+package intelegencia.vivek.intelegencia.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static Retrofit retrofit = null;
+    static String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+
+
+        return retrofit;
+    }
+
+}
